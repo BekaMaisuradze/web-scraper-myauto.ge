a47662528966927085dc826e2e4f38dd48df9ae6
beqa <bmais14@freeuni.edu.ge>
2020-04-26 16:07:30 +0400

    Extract car price

--- MyAutoScraper/spiders/myauto.py
+++ MyAutoScraper/spiders/myauto.py
@@ -6,11 +6,13 @@ class MyautoSpider(scrapy.Spider):
     allowed_domains = ['myauto.ge']
     start_urls = ['https://www.myauto.ge/en/s/00/0/00/00/00/00/00/cars?stype=0&currency_id=3&det_search=0&ord=7&category_id=m0&keyword=']
     pages_left = 1
+    fields = ['Manufacturer', 'Model', 'Category', 'Mileage', 'Gear box type',
+            'Doors', 'Wheel', 'Color', 'Interior color', 'VIN', 'Leather interior']
 
     def parse(self, response):
 
         detail_page_links = response.css('div.car-name-left a::attr(href)').getall()
-        yield from response.follow_all(detail_page_links[:1], self.parse_announcement)
+        yield from response.follow_all(detail_page_links, self.parse_announcement)
 
         self.pages_left -= 1
         if self.pages_left > 1:
@@ -27,8 +29,9 @@ class MyautoSpider(scrapy.Spider):
             divs = soup.find_all('div')
             if (len(divs) == 2):
                 key = divs[0].string.replace('\xa0', ' ').strip()
-                val = divs[1].h2.string if divs[1].h2 else divs[1].string
-                res[key] = ' '.join(val.replace('\xa0', ' ').split()) if val else ''
+                if (key in self.fields):
+                    val = divs[1].h2.string if divs[1].h2 else divs[1].string
+                    res[key] = ' '.join(val.replace('\xa0', ' ').split()) if val else ''
 
         rights = response.css('table.detail-car-table tr th.th-right')
         for item in rights:
@@ -36,10 +39,12 @@ class MyautoSpider(scrapy.Spider):
             divs = soup.find_all('div')
             if (len(divs) == 2 and divs[1].i.has_attr('class')):
                 key = divs[0].string.replace('\xa0', ' ').strip()
-                val = 1 if divs[1].i['class'][1] == 'fa-check' else 0
-                res[key] = val
+                if (key in self.fields):
+                    val = 1 if divs[1].i['class'][1] == 'fa-check' else 0
+                    res[key] = val
 
         res['Customs cleared'] = 1 if len(response.css('.levy-true')) > 0 else 0
+        res['Price'] = response.css('span.car-price::text').getall()[-1].strip()
 
         res['imgs'] = []
         imgs = response.css('div.thumbnail-image img')
